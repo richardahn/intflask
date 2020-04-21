a0625fe69e1db83b815f147359b48914cfd902ad
Richard Ahn <richahn2@gmail.com>
2020-04-20 20:34:26 -0400

    working update

--- client/src/components/IntflaskEditor.js
+++ client/src/components/IntflaskEditor.js
@@ -1,7 +1,17 @@
-import React, { useMemo, useState, useCallback } from 'react';
-import { createEditor } from 'slate';
-import { Editable, withReact, Slate } from 'slate-react';
+import React, {
+  useMemo,
+  useState,
+  useCallback,
+  useRef,
+  useEffect,
+} from 'react';
+import { Editor, createEditor } from 'slate';
+import { Editable, withReact, Slate, useSlate } from 'slate-react';
 import { withHistory } from 'slate-history';
+import { connect } from 'react-redux';
+
+// Redux
+import { setEditorState } from '../actions/editor';
 
 // Intflask Slate Tools
 import withShortcuts from '../utils/intflask-slate/shortcuts';
@@ -9,8 +19,37 @@ import keyDownHandler from '../utils/intflask-slate/keyDownHandler';
 import Element from '../utils/intflask-slate/element';
 import Leaf from '../utils/intflask-slate/leaf';
 
-export default function IntflaskEditor() {
-  const [value, setValue] = useState(initialValue);
+function SideblockColumn({ editorDomNodes, ...rest }) {
+  const editor = useSlate();
+  console.log('in sideblock...');
+  console.log(editorDomNodes);
+  console.log(editor.children);
+  const sideblocks = editorDomNodes.map((node, i) => {
+    const slateNode = editor.children[i];
+    const style = node.currentStyle || window.getComputedStyle(node);
+    console.log(`Top: ${style.marginTop}, height: ${node.offsetHeight}`);
+    return (
+      <div
+        style={{
+          marginTop: style.marginTop,
+          height: node.offsetHeight,
+        }}
+        className="col s12"
+      >
+        {slateNode?.sideblock?.text}
+      </div>
+    );
+  });
+  return <div {...rest}>{sideblocks}</div>;
+}
+
+const IntflaskEditable = React.forwardRef((props, ref) => (
+  <div ref={ref}>
+    <Editable {...props} />
+  </div>
+));
+
+function IntflaskEditor(props) {
   const editor = useMemo(
     () => withShortcuts(withHistory(withReact(createEditor()))),
     [],
@@ -21,53 +60,64 @@ export default function IntflaskEditor() {
     editor,
   ]);
 
+  const [editorDom, setEditorDom] = useState(null);
+  const editorRef = useCallback((dom) => {
+    setEditorDom(dom);
+    debugger;
+  });
+
+  const [editorDomNodes, setEditorDomNodes] = useState([]);
+  useEffect(() => {
+    if (editorDom != null) {
+      setEditorDomNodes([
+        ...editorDom.querySelectorAll('[data-slate-node="element"]'),
+      ]);
+    }
+  }, [props.editor, editorDom]);
+  // when state changes, update dom, but dom hasnt updated yet
+  // so update dom when ref changes, but ref doesnt represent all children
+  // Slate state changes when ur SELECTION changes
+  // If this then changes editor state with redux
+  // However, the actual slate json content didn't change so our component
+  // doesnt rerender
+  let sideblocks = null;
+  if (editorDomNodes != null) {
+    sideblocks = (
+      <SideblockColumn className="col s6" editorDomNodes={editorDomNodes} />
+    );
+  }
+  debugger;
   return (
-    <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
-      <Editable
+    <Slate
+      editor={editor}
+      value={props.editor}
+      onChange={(value) => props.setEditorState(value)}
+    >
+      <IntflaskEditable
+        ref={editorRef}
         renderElement={renderElement}
         renderLeaf={renderLeaf}
         onKeyDown={onKeyDown}
         placeholder="Enter some rich text…"
         spellCheck={false}
         autoFocus
+        className="col s6"
+        style={{
+          backgroundColor: 'lightblue',
+        }}
       />
+      {sideblocks}
     </Slate>
   );
 }
 
-const initialValue = [
-  {
-    type: 'paragraph',
-    children: [
-      { text: 'This is editable ' },
-      { text: 'rich', bold: true },
-      { text: ' text, ' },
-      { text: 'much', italic: true },
-      { text: ' better than a ' },
-      { text: '<textarea>', code: true },
-      { text: '!' },
-    ],
-  },
-  {
-    type: 'paragraph',
-    children: [
-      {
-        text:
-          "Since it's rich text, you can do things like turn a selection of text ",
-      },
-      { text: 'bold', bold: true },
-      {
-        text:
-          ', or add a semantically rendered block quote in the middle of the page, like this:',
-      },
-    ],
-  },
-  {
-    type: 'block-quote',
-    children: [{ text: 'A wise quote.' }],
-  },
-  {
-    type: 'paragraph',
-    children: [{ text: 'Try it out for yourself!' }],
-  },
-];
+// Maps store state to props
+const mapStateToProps = (state) => ({
+  editor: state.editor,
+});
+
+const mapDispatchToProps = {
+  setEditorState,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(IntflaskEditor);
---
+++ client/src/reducers/editor.js
@@ -0,0 +1,52 @@
+import { SET_EDITOR_STATE } from '../actions/editor';
+
+const initialState = [
+  {
+    type: 'paragraph',
+    children: [
+      { text: 'This is editable ' },
+      { text: 'rich', bold: true },
+      { text: ' text, ' },
+      { text: 'much', italic: true },
+      { text: ' better than a ' },
+      { text: '<textarea>', code: true },
+      { text: '!' },
+    ],
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text:
+          "Since it's rich text, you can do things like turn a selection of text ",
+      },
+      { text: 'bold', bold: true },
+      {
+        text:
+          ', or add a semantically rendered block quote in the middle of the page, like this:',
+      },
+    ],
+    sideblock: {
+      text: 'Since its rich text!',
+    },
+  },
+  {
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }],
+  },
+  {
+    type: 'paragraph',
+    children: [{ text: 'Try it out for yourself!' }],
+    sideblock: {
+      text: 'Since its rich text!',
+    },
+  },
+];
+export default function editor(state = initialState, action) {
+  switch (action.type) {
+    case SET_EDITOR_STATE:
+      return action.state;
+    default:
+      return state;
+  }
+}
